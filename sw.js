966ec832234f9c25975c21d98cca705c5abd43b6
mayowaakindebe-creator <mayowaakindebe@gmail.com>
2026-05-14 22:08:21 -0400

    new logo and name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'roam-v8';
+const CACHE = 'roam-v10';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
