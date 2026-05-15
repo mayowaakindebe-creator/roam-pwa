f8f0fece1627a3f9d438c316f29fd2ff2514dc54
mayowaakindebe-creator <mayowaakindebe@gmail.com>
2026-05-14 21:29:55 -0400

    API UPdate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'roam-v3';
+const CACHE = 'roam-v7';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
